db68112d27bf38fe44ff8ac033ef27111a02fa2f
Slava <vs.petrochenko@gmail.com>
2018-01-13 10:10:57 +0200

    added feed adapter/vh

---
+++ app/src/main/java/com/example/scame/constraintlayoutbenchmarks/FeedAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.scame.constraintlayoutbenchmarks
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+
+class FeedAdapter: RecyclerView.Adapter<FeedItemViewHolder>() {
+
+    override fun onBindViewHolder(holder: FeedItemViewHolder?, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): FeedItemViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
---
+++ app/src/main/java/com/example/scame/constraintlayoutbenchmarks/FeedItemViewHolder.kt
@@ -0,0 +1,22 @@
+package com.example.scame.constraintlayoutbenchmarks
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.TextView
+
+class FeedItemViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    private val mainTitle = itemView.findViewById<TextView>(R.id.main_title)
+    private val hoursTv = itemView.findViewById<TextView>(R.id.hours_tv)
+    private val shortDescription = itemView.findViewById<TextView>(R.id.short_description)
+    private val overlayText = itemView.findViewById<TextView>(R.id.overlay_text)
+    private val articleStartText = itemView.findViewById<TextView>(R.id.article_start_text)
+    private val communityName = itemView.findViewById<TextView>(R.id.community_name)
+    private val emmotionsNumber = itemView.findViewById<TextView>(R.id.emotions_number)
+    private val commentsNumber = itemView.findViewById<TextView>(R.id.comments_number)
+    private val sharesNumber = itemView.findViewById<TextView>(R.id.shares_number)
+
+    fun bind() {
+
+    }
+}
